0b587dc82fbaeccfd055c122ed342c705083ce6b
fadyehabamer <fadyamer45@gmail.com>
2022-02-16 15:25:11 +0200

    let var const compare

---
+++ Let,Var,Const Comparison/localScope.js
@@ -0,0 +1,19 @@
+//  * Local Scope  
+// * Let and Const have same behaviour
+
+// * Global scope is available for all
+// * Block Scope  is Private
+
+let x = 20
+console.log(x); // * 20
+
+{
+    let x = 30;
+    console.log(x); // * 30
+}
+
+(function logX (){
+    let x = 40 ; 
+    console.log(x); // * 40
+}()) 
+// * IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined
