3ad99430e30648baaccbe9795ba78d1241797b99
fadyehabamer <fadyamer45@gmail.com>
2022-02-23 21:19:06 +0200

    closures

---
+++ Core JS/Closures/main.js
@@ -0,0 +1,8 @@
+// * Closure
+/* A closure is the combination of a function bundled together 
+with references to its surrounding state (the lexical environment). 
+In other words, a closure gives you access to an outer function's scope 
+from an inner function. 
+In JavaScript,
+closures are created every time a function is created, at function creation time
+*/
