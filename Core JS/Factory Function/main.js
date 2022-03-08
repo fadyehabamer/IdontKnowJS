f49fa7e51c66daac50ce22ab26e9fa950fe57512
fadyehabamer <fadyamer45@gmail.com>
2022-03-08 22:19:19 +0200

    Factory Function

--- Core JS/Factory Function/main.js
+++ Core JS/Factory Function/main.js
@@ -4,3 +4,9 @@ The Factory Function is similar to constructor functions/class functions,
 but instead of using new to create an object,
 factory functions simply creates an object and returns it.
 */
+
+/*
+but they do not require the use of the ‘this‘ keyword
+for inner values or the use of the ‘new‘ keyword
+when instantiating new objects
+*/
