f5eca53f4ecee5f225e5de17fceaa3d783ecbedc
fadyehabamer <fadyamer45@gmail.com>
2022-03-30 15:37:43 +0200

    Update main.js

--- Core JS/Remove Falsy Values/main.js
+++ Core JS/Remove Falsy Values/main.js
@@ -1,11 +1,30 @@
 let my_arr = ['fady' , undefined , 'amer']
+
+function removeFalsey(arr) {
+    // newly created array
+    let newArr = [];
+ 
+    // Iterate the array using the forEach loop
+    arr.forEach((k) => {
+      // check for the truthy value
+      if (k) {
+        newArr.push(k);
+      }
+    });
+    // return the new array
+    return newArr;
+  }
+ 
+ console.log(removeFalsey(arr));
+
+
 let removedFalsyValues = my_arr.filter((item)=>{return !(!item)})
 let removedFalsyValues_2 = my_arr.filter((item)=>{return Boolean(item)})
 
 
 console.log(Boolean('fady'));
-// undefined == false
-// console.log(!undefined);   //true
-// console.log(!'fady')      // false
-// console.log(my_arr);
+undefined == false
+console.log(!undefined);   //true
+console.log(!'fady')      // false
+console.log(my_arr);
 console.log(removedFalsyValues_2);
