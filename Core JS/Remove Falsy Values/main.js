81ef8090562d73e0c0f4ab07fbcb0c5dabe0a76e
fadyehabamer <fadyamer45@gmail.com>
2022-03-30 15:39:11 +0200

    Remove Falsy Values

--- Core JS/Remove Falsy Values/main.js
+++ Core JS/Remove Falsy Values/main.js
@@ -1,25 +1,25 @@
-let my_arr = ['fady' , undefined , 'amer']
+let my_arr = ['fady', undefined, 'amer']
 
 function removeFalsey(arr) {
-    // newly created array
-    let newArr = [];
- 
-    // Iterate the array using the forEach loop
-    arr.forEach((k) => {
-      // check for the truthy value
-      if (k) {
-        newArr.push(k);
-      }
-    });
-    // return the new array
-    return newArr;
-  }
- 
- console.log(removeFalsey(arr));
+  // newly created array
+  let newArr = [];
+
+  // Iterate the array using the forEach loop
+  arr.forEach((k) => {
+    // check for the truthy value
+    if (k) {
+      newArr.push(k);
+    }
+  });
+  // return the new array
+  return newArr;
+}
+
+console.log(removeFalsey(my_arr));
 
 
-let removedFalsyValues = my_arr.filter((item)=>{return !(!item)})
-let removedFalsyValues_2 = my_arr.filter((item)=>{return Boolean(item)})
+let removedFalsyValues = my_arr.filter((item) => { return !(!item) })
+let removedFalsyValues_2 = my_arr.filter((item) => { return Boolean(item) })
 
 
 console.log(Boolean('fady'));
@@ -27,4 +27,5 @@ undefined == false
 console.log(!undefined);   //true
 console.log(!'fady')      // false
 console.log(my_arr);
+console.log(removedFalsyValues);
 console.log(removedFalsyValues_2);
