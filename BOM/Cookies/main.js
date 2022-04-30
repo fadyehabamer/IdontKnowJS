9fbeb2a1af796a23e9fc70238e6dfdda0e0129e0
fadyehabamer <fadyamer45@gmail.com>
2022-05-01 00:18:41 +0200

    cookies

---
+++ BOM/Cookies/main.js
@@ -0,0 +1,16 @@
+// Cookies
+// document.cookie = (
+// "name=value;
+// expires=date; 
+// path=path; 
+// domain=domain; 
+// secure";)
+
+document.cookie = "fName:Fady; expires=Sun, 1 Jan 2023 12:00:00 UTC; path=/";
+// document.cookie = "lName:Amer; path=/";
+
+// modify the cookie
+document.cookie = "fName:FADY; expires=Sun, 1 Jan 2023 12:00:00 UTC; path=/";
+
+// delete cookie
+document.cookie = "fName; expires=Mon, 1 Jan 2022 12:00:00 UTC; path=/"; 
